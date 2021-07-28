a85cefa9c9b17529353bda724482e5ff641e0967
Arkadii Shvartsman <a.shvartsman@corp.mail.ru>
2021-07-29 00:28:43 +0300

    Initial commit

---
+++ fs.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+func readFIle(fileName string) string {
+	// Open file for reading
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(data)
+}
+
+func replaceTasks(text string) string {
+	return strings.ReplaceAll(text, "task_" ,"https://jira.mail.ru/browse/" )
+}
+
+func WriteFile(filename string, message string) {
+	err := os.WriteFile("task/"+strings.ReplaceAll(filename, "/", "_"), []byte(replaceTasks(message)), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func DeleteAllFiles() {
+	var files, _ = os.ReadDir("task")
+	for _, f := range files {
+		os.Remove("task/" + f.Name())
+	}
+}
+func allFilesData() string {
+	var files, _ = os.ReadDir("task")
+	var res = ""
+	for _, f := range files {
+		res += replaceTasks(f.Name()) + " : " + readFIle("task/" + f.Name()) + "\n"
+	}
+	return res
+}
+
+func GoDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	println(os.Getenv(key), key)
+	return os.Getenv(key)
+}
---
+++ index.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"github.com/mail-ru-im/bot-golang"
+	"log"
+)
+
+func main() {
+	bot, err := botgolang.NewBot(
+		GoDotEnvVariable("TOKEN"),
+		botgolang.BotApiURL("https://api.internal.myteam.mail.ru/bot/v1"),
+	)
+
+	if err != nil {
+		log.Fatalf("cannot connect to bot: %s", err)
+	}
+
+	log.Println(bot.Info)
+	var account = GoDotEnvVariable("account")
+	var ctx = context.Background()
+	updates := bot.GetUpdatesChannel(ctx)
+	var fileNow = ""
+	for update := range updates {
+		// fmt.Println(update.Type, update.Payload)
+		switch update.Type {
+		case botgolang.NEW_MESSAGE:
+			message := update.Payload.Message()
+			if fileNow == "true" {
+				fileNow = message.Text
+				message := bot.NewMessage(account)
+				message.Text = "now type a res"
+				err := message.Send()
+				if err != nil {
+					log.Fatal(err)
+				}
+			} else if fileNow != "" {
+				WriteFile(fileNow, message.Text)
+				fileNow = ""
+				message := bot.NewMessage(account)
+				message.Text = "all done"
+				err := message.Send()
+				if err != nil {
+					log.Fatal(err)
+				}
+			} else {
+				var chat = update.Payload.Chat.ID
+				addTSK := botgolang.NewCallbackButton("add task", "task")
+				resBrn := botgolang.NewCallbackButton("Result of the week", "res")
+				message := bot.NewMessage(chat)
+				keyboard := botgolang.NewKeyboard()
+				keyboard.AddRow(
+					addTSK,
+					resBrn,
+				)
+				message.AttachInlineKeyboard(keyboard)
+				message.Text = " "
+				if err := message.Send(); err != nil {
+					log.Printf("failed to send message: %s", err)
+				}
+			}
+		case botgolang.CALLBACK_QUERY:
+			data := update.Payload.CallbackQuery()
+			if data.CallbackData == "res" {
+				err := bot.SendMessage(bot.NewTextMessage(
+					account,
+					allFilesData(),
+				))
+				if err != nil {
+					log.Fatal(err)
+				} else {
+					DeleteAllFiles();
+				}
+			} else {
+				fileNow = "true"
+				message := bot.NewMessage(account)
+				message.Text = "type task name"
+				err := message.Send()
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
+
+	}
+}
+
